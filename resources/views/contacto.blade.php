8086939c6b008c1d6c1fb0401e64f1b998d58177
Marco Morales <marcomorales.mm41@gmail.com>
2022-09-07 10:57:11 -0500

    Css y vistas

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Contacto</title>
+    </head>
+
+    <body>
+
+    <form action="formulario.html" method="get">
+
+        <label for="nombre">Nombre: </label><input type="text" name="Nombre" id="nombre"><br>
+        <label for="correo"">Correo: </label><input type="email" name="correo" id="mail"><br>
+        <label for="comentario"">Comentario: </label><br><textarea name="comentario"  rows="6" cols="0"></textarea><br>
+
+
+    </form>
+
+    </body>
+</html>
---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Inicio</title>
+    </head>
+
+    <body>
+        <link rel="stylesheet" href="style.css">
+        <h1>LandingPage</h1>
+
+    </body>
+</html>
